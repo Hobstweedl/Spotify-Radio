3919fa13afa417075dba8c9a8d5da98b4ac99080
Jesse Rice <hobstweedl@gmail.com>
2015-05-22 16:07:33 -0400

    Syncing seek times for new clients

--- authorization_code/public/app.js
+++ authorization_code/public/app.js
@@ -11,10 +11,34 @@
     AUDIO= myAudio;
     var totalTime;
     var lastVolume = 0;
-
-    
     AUDIO.volume = 0.75;
-    AUDIO.addEventListener("timeupdate", progress, false);
+
+    function getTime(t) {
+      var m=~~(t/60), s=~~(t % 60);
+      return (m<10?"0"+m:m)+':'+(s<10?"0"+s:s);
+    }
+    
+    function progress() {
+      $bar.slider('value', ~~(100/AUDIO.duration*AUDIO.currentTime));
+      $pp.text(getTime(AUDIO.currentTime) + '/' + totalTime);
+    }
+
+    /*
+      | Emit
+
+      Will create a stream and send json string in the meta. Stream is empty
+      Since this is using the client.send command, a new stream is created
+
+      Stream may have to be killed in the Stream:end event
+
+    */
+
+    function emit(action, time) {
+      var data = {};
+      data["event"] = action;
+      data["time"] = time;
+      return client.send({}, JSON.stringify(data) );
+    }
     
     
 
@@ -43,23 +67,11 @@
       
     });
 
-    function getTime(t) {
-      var m=~~(t/60), s=~~(t % 60);
-      return (m<10?"0"+m:m)+':'+(s<10?"0"+s:s);
-    }
-    
-    function progress() {
-      $bar.slider('value', ~~(100/AUDIO.duration*AUDIO.currentTime));
-      $pp.text(getTime(AUDIO.currentTime) + '/' + totalTime);
-    }
+    /*
 
-    function emit(action, time) {
-      var data = {};
-      data["event"] = action;
-      data["time"] = time;
-      console.log( JSON.stringify(data) );
-      return client.send({}, JSON.stringify(data) );
-    }
+    Event Listeners
+
+    */
 
     myAudio.addEventListener('timeupdate', progress, false);
 
@@ -73,22 +85,48 @@
     myAudio.addEventListener('canplay', function(e){
       // Audio is ready, ask the server for the time to go to
       emit('seek');
+      myAudio.play();
     });
+
+    myAudio.addEventListener('ended', function(e){
+      emit('songend');
+    });
+
 
     client.on('open', function(s,m){
       console.log('connection established');
       emit('startsong');
     });
+
+    /*
+
+    Client and Stream events
+
+    */
     
 
 
     // Received new stream from server!
-    client.on('stream', function(stream, meta){   
-        var parts = [];
+    client.on('stream', function(stream, meta){  
+         var parts = [];
+
+        if(meta == 'seek'){
+          console.log('I am returning my seektime');
+          emit('seekReturn', myAudio.currentTime);
+        }
+
+
 
         stream.on('data', function(data){
-          console.log('loading data');
-          parts.push(data);
+
+          if(meta == 'playsong'){
+            console.log('play song');
+            console.log(data);
+          } else{
+            console.log('loading data');
+            parts.push(data);
+          }
+          
         });
 
         stream.on('end', function(){
@@ -97,14 +135,13 @@
           myAudio.preload="";        
 
           source.connect( audioCtx.destination );
-          myAudio.volume = 0.75;
-          myAudio.play();
+          myAudio.volume = 0.0;
+
+          stream.destroy();
+          
         });
 
-        myAudio.addEventListener('ended', function(e){
-          console.log('song has finished, emit event');
-          emit('song_ended');
-        });
+       
 
 
     });
--- authorization_code/stream.js
+++ authorization_code/stream.js
@@ -4,32 +4,58 @@ var fs = require('fs');
 var tdwp = ['../music/tdwp/escape.mp3', '../music/tdwp/anatomy.mp3', '../music/tdwp/outnumbered.mp3']
 
 var server = BinaryServer({ port: 9000, chunkSize: 100 });
-var numberofClients = 0;
+var seektimes = [];
+var clients = [];
+var seekreturnids = []
 
 server.on('connection', function(client){
-	numberofClients++;
+    clients.push(client);
 
     client.on('stream', function(stream, meta){
     	var metadata = JSON.parse(meta);
-    	console.log(metadata.event);
 
     	switch(metadata.event){
     		case 'startsong':
     			console.log('New connection, launch song in progress');
     			var file = fs.createReadStream('../music/tdwp/escape.mp3');
-    			client.send(file);
+    			client.send(file, 'startsong');
     		break;
 
     		case 'songend':
-    			console.log('Song ended, start new song');
     		break;
 
     		case 'seek':
-    			console.log('Poll connections for seek time');
+                for(var i = 0; i < clients.length; i++){
+                    if(clients[i].id !== client.id){
+                        clients[i].send({}, 'seek');
+                    }
+                }
     		break;
 
+            case 'seekReturn':
+                seektimes.push(metadata.time);
+                seekreturnids.push(client.id);
+                var sum = 0
+                var l = seektimes.length;
+                for( var j = 0; j < l; j++){
+                    sum += seektimes[j];
+                }
+                var avg = sum/l;
+                for(var i = 0; i < clients.length; i++){
+                    var success = seekreturnids.indexOf( clients[i].id);
+
+                    if(success == -1){
+                        clients[i].send(avg, 'playsong');
+                    }
+                }
+
+                seekreturnids = [];
+                seektimes = [];
+                
+            break;
+
     		default: 
-    			console.log('could not find action');
+    			//console.log('could not find action');
     		break;
     	}
         
@@ -37,10 +63,14 @@ server.on('connection', function(client){
 
     client.on('close', function(stream, meta){
     	console.log('Connection Terminated');
-    	numberofClients--;
+        for( var i = 0; i < clients.length; i++){
+            if(client.id == clients[i].id ){
+                clients.splice(i, 1);
+            }
+        }
     });
 
     //var file = fs.createReadStream('../music/tdwp/escape.mp3');
     //client.send(file);
-    console.log('Connection Started');
+    //console.log('Connection Started');
 });
