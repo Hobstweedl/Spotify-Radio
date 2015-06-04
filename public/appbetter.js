64787380aa16f1985d7b988f13a5de23702ddb88
Jesse Rice <hobstweedl@gmail.com>
2015-06-04 17:14:37 -0400

    Going with ReactJS for panels

--- public/appbetter.js
+++ public/appbetter.js
@@ -8,20 +8,6 @@ var $pp  = $('#playpause'),
 $vol = $('#volume');
 var lastVolume = 0;
 
-function compilePanel(title, items, id){
-  var htmlList = '<div class="panel panel-info" id="' + id + '">' +
-          '<div class="panel-heading">' +
-              '<h3 class="panel-title"> ' + title +' </h3>' +
-              '<span class="pull-right clickable" data-effect="slideUp"><i class="fa fa-times"></i></span>' +
-          '</div>' +
-          '<div class="panel-body">' + items + 
-          '</div>'+
-        '</div>';
-
-  return htmlList;
-}
-
-
 function getTime(t) {
   var m=~~(t/60), s=~~(t % 60);
   return (m<10?"0"+m:m)+':'+(s<10?"0"+s:s);
@@ -56,14 +42,7 @@ $vol.slider( {
 
 */
 
-$.getJSON( "/api/artists", function( data ) {
-  var items = [];
-  $.each( data, function( key, val ) {
-    items.push( '<a class="list-group-item" data-type="artist" data-artist="' + val + '">' + val + "</a>" );
-  });
 
-  $( items.join( "" ) ).appendTo( "#base" );
-});
 
 
 $pp.click(function() {
@@ -98,76 +77,6 @@ $('button.seat-btn').on('click', function(){
   socket.emit('seat request');
 });
 
-$(document).on('click', '.list-group-item', function(){
-  var jdata = {};
-  var artist = $(this).data('artist');
-  var album = $(this).data('album');
-  var t = $(this).data('id')
-  var type = $(this).data('type');
-  var trackSelected = false;
-
-  switch(type) {
-      case 'artist':
-        jdata.url = '/api/artist/' + artist;
-        jdata.type = 'album'
-        jdata.item = artist
-        jdata.div = 'album-panel'
-        $("#base-panel").hide();
-      break;
-
-      case 'album':
-      console.log('enter album');
-        jdata.url = '/api/artists/' + artist + '/album/' + album;
-        jdata.type = 'track'
-        jdata.item = album
-        jdata.div = 'track-panel'
-        $("#album-panel").hide();
-      break
-
-      case 'track':
-        trackSelected = true;
-      break;
-
-  }
-  
-
-  if(trackSelected == false){
-    var compile;
-    $.getJSON( jdata.url, function( data ) {
-      var items = '';
-      $.each( data, function( key, val ) {
-
-        if(jdata.type== 'album'){
-          items += '<a class="list-group-item" data-type="' + jdata.type +
-           '" data-artist="' + artist + '" data-album="' + val + '">' + val + "</a>";
-        } else{
-          items += '<a class="list-group-item" data-type="' + jdata.type + '" data-id="' + val._id + '">' + val.title + "</a>";
-        }
-        
-      });
-
-      compile = compilePanel(jdata.item, items, jdata.div);
-      $( compile ).appendTo( ".left-window" );
-
-    });
-  } else{
-    socket.emit('queue track', {trackID : t});
-  }
-
-});
-
-
-$(document).on('click', '.clickable',function(){
-  var effect = $(this).data('effect');
-  var panel = $(this).closest('.panel')[effect]();
-  if(panel[0].id == 'album-panel'){
-    $("#base-panel").show();
-  }
-
-  if(panel[0].id == 'track-panel'){
-    $("#album-panel").show();
-  }
-});
 
 
 
